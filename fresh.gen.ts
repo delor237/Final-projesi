b6c81230b10befa7a458fea9a8bc8b6476da4685
delor237 <delorakenmoe@gmail.com>
2026-05-26 17:08:21 +0300

    chore:update-generated-files

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,6 +5,7 @@
 import * as $_404 from "./routes/_404.tsx";
 import * as $_500 from "./routes/_500.tsx";
 import * as $_app from "./routes/_app.tsx";
+import * as $_layout from "./routes/_layout.tsx";
 import * as $_middleware from "./routes/_middleware.ts";
 import * as $api_categories from "./routes/api/categories.ts";
 import * as $api_todos from "./routes/api/todos.ts";
@@ -25,6 +26,7 @@ const manifest = {
     "./routes/_404.tsx": $_404,
     "./routes/_500.tsx": $_500,
     "./routes/_app.tsx": $_app,
+    "./routes/_layout.tsx": $_layout,
     "./routes/_middleware.ts": $_middleware,
     "./routes/api/categories.ts": $api_categories,
     "./routes/api/todos.ts": $api_todos,
